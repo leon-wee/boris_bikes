1e602641da8096afe5b03f43a21b79a3d1317a11
Leon Wee <leonwee27@gmail.com>
2015-08-27 16:00:08 +0700

    Added more tests for robustness

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -9,19 +9,33 @@ describe Bike do
   end
 
   context '#Methods' do
-    it 'checks if bike is working' do
-      expect(subject.working?).to eq(true)
-    end
-
     it 'can be reported as broken' do
       subject.report_broken
       expect(subject.status).to eq(false)
     end
 
-    it 'checks if is broken' do
-      subject.report_broken
-      expect(subject.broken?).to eq(true)
+    context 'when bike is broken' do
+      it 'check working returns false' do
+        subject.report_broken
+        expect(subject.working?).to eq(false)
+      end
+
+      it 'check broken returns true' do
+        subject.report_broken
+        expect(subject.broken?).to eq(true)
+      end
     end
+
+    context 'when bike is working' do
+      it 'check working returns true' do
+        expect(subject.working?).to eq(true)
+      end
+
+      it 'check broken returns false' do
+        expect(subject.broken?).to eq(false)
+      end
+    end
+
   end
 
 end
